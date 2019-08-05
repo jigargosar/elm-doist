a1027f4dbdf051efc4f98a7c2f26eefaf2c8dfbe
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-05 15:34:02 +0530

    auth

--- src/index.js
+++ src/index.js
@@ -13,7 +13,17 @@ const app = Elm.Main.init({
 })
 
 
-firebase.initializeApp({})
+const firebaseConfig = {
+  apiKey: "AIzaSyBVS1Tx23pScQz9w4ZDTGh307mqkCRy2Bw",
+  authDomain: "not-now-142808.firebaseapp.com",
+  databaseURL: "https://not-now-142808.firebaseio.com",
+  projectId: "not-now-142808",
+  storageBucket: "not-now-142808.appspot.com",
+  messagingSenderId: "476064436883",
+  appId: "1:476064436883:web:bcd2d5b958a90fa6"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 firebase.auth().onAuthStateChanged(user =>{
   if(user){
@@ -21,7 +31,6 @@ firebase.auth().onAuthStateChanged(user =>{
   }else{
     app.ports.onAuthStateChanged.send(null)
   }
-
 })
 
 
