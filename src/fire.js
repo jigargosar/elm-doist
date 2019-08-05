6ec2277f74276b0fbf580c7d8e9de249737b6566
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-05 20:46:36 +0530

    todo collection

--- src/fire.js
+++ src/fire.js
@@ -3,67 +3,59 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import { invariant } from './invariant'
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBVS1Tx23pScQz9w4ZDTGh307mqkCRy2Bw",
-  authDomain: "not-now-142808.firebaseapp.com",
-  databaseURL: "https://not-now-142808.firebaseio.com",
-  projectId: "not-now-142808",
-  storageBucket: "not-now-142808.appspot.com",
-  messagingSenderId: "476064436883",
-  appId: "1:476064436883:web:bcd2d5b958a90fa6"
-};
+  apiKey: 'AIzaSyBVS1Tx23pScQz9w4ZDTGh307mqkCRy2Bw',
+  authDomain: 'not-now-142808.firebaseapp.com',
+  databaseURL: 'https://not-now-142808.firebaseio.com',
+  projectId: 'not-now-142808',
+  storageBucket: 'not-now-142808.appspot.com',
+  messagingSenderId: '476064436883',
+  appId: '1:476064436883:web:bcd2d5b958a90fa6',
+}
 
 firebase.initializeApp(firebaseConfig)
-
 
 function Disposables() {
   const disposables = []
   return {
-    add(fn){
+    add(fn) {
       disposables.push(fn)
     },
-    dispose(){
+    dispose() {
       disposables.forEach(fn => fn())
       disposables.splice(0, disposables.length)
-    }
+    },
   }
 }
-
 
 export function Fire() {
   const auth = firebase.auth()
   const db = firebase.firestore()
   const authChangeDisposables = Disposables()
 
-  auth.onAuthStateChanged(user=>{
+  auth.onAuthStateChanged(user => {
     authChangeDisposables.dispose()
   })
   return {
-    onAuthStateChanged(cb){
+    onAuthStateChanged(cb) {
       return auth.onAuthStateChanged(cb)
     },
-    signIn(){
+    signIn() {
       const provider = new firebase.auth.GoogleAuthProvider()
-      provider.setCustomParameters({prompt:'select_account'})
+      provider.setCustomParameters({ prompt: 'select_account' })
       return auth.signInWithPopup(provider)
     },
     signOut() {
       return auth.signOut()
     },
-    disposeOnAuthChange(fn){
+    disposeOnAuthChange(fn) {
       authChangeDisposables.add(fn)
     },
-    userCRef(name){
+    userCRef(name) {
       const uid = auth.currentUser.uid
       invariant(name.trim().length > 0)
       invariant(uid.trim().length > 0)
-      return  db.collection(`users/${uid}/${name}`)
-    }
+      return db.collection(`users/${uid}/${name}`)
+    },
   }
 }
-
-
-
-
-
