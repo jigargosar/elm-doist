d448513fd788a570019434460844755e5a98b245
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-05 16:44:31 +0530

    fire

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css'
 import { Elm } from './Main.elm'
 import { Fire } from './fire'
 
+import { mapObjIndexed , identity} from 'ramda'
+
 const app = Elm.Main.init({
   flags: {
     modelCache: JSON.parse(localStorage.getItem('modelCache') || 'null'),
@@ -11,46 +13,54 @@ const app = Elm.Main.init({
 })
 const fire = Fire()
 
-const pub = {
-  onAuthStateChanged: (arg) => {
-    const portName = 'onAuthStateChanged'
-    const send = path(['ports', portName, 'send'])(app)
-    if (!send) {
-      console.warn('Send: Port Not Found:', portName, arg)
-      return
-    }
-    if(send){
-      send(arg)
-    }
-  }
-}
+
+
+const pubs = initPubs({
+  onAuthStateChanged: identity
+})
 
 fire.onAuthStateChanged(user =>{
   if(user){
-    pub.onAuthStateChanged(user.uid)
+    pubs.onAuthStateChanged(user.uid)
   }else{
-    pub.onAuthStateChanged(null)
+    pubs.onAuthStateChanged(null)
   }
 })
 
 
 
 
-
-
-const subs = {
+initSubs({
   cacheKeyValue: ([k, v]) => {
     console.log('cacheKeyValue', k, v)
     localStorage.setItem(k, JSON.stringify(v))
   },
+})
+
+
+function initSubs(subs) {
+  forEachObjIndexed((listener, portName) => {
+    const subscribe = path(['ports', portName, 'subscribe'])(app)
+    if (!subscribe) {
+      console.warn('Subscribe: Port Not Found:', portName)
+      return
+    }
+    console.log('Subscription Port Attached', portName)
+    subscribe(listener)
+  })(subs)
 }
 
-forEachObjIndexed((listener, portName) => {
-  const subscribe = path(['ports', portName, 'subscribe'])(app)
-  if (!subscribe) {
-    console.warn('Subscribe: Port Not Found:', portName)
-    return
-  }
-  console.log('Subscription Port Attached', portName)
-  subscribe(listener)
-})(subs)
+function initPubs(pubs) {
+  return mapObjIndexed((fn, portName)=>{
+    return (arg) => {
+      const send = path(['ports', portName, 'send'])(app)
+      if (!send) {
+        console.warn('Send: Port Not Found:', portName, arg)
+        return
+      }
+      if(send){
+        send(arg)
+      }
+    }
+  })(pubs)
+}
