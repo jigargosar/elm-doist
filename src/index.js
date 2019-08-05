a586bb1a3396f009d23e4a540e334172c951de1e
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-05 15:00:29 +0530

    auth

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import forEachObjIndexed from 'ramda/es/forEachObjIndexed'
 import path from 'ramda/es/path'
 import './index.css'
 import { Elm } from './Main.elm'
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
 
 const app = Elm.Main.init({
   flags: {
     modelCache: JSON.parse(localStorage.getItem('modelCache') || 'null'),
   },
 })
+
+
+firebase.initializeApp({})
+
+firebase.auth().onAuthStateChanged(user =>{
+  if(user){
+    app.ports.onAuthStateChanged.send(user.uid)
+  }else{
+    app.ports.onAuthStateChanged.send(null)
+  }
+
+})
+
+
+
+
+
 
 const subs = {
   cacheKeyValue: ([k, v]) => {
