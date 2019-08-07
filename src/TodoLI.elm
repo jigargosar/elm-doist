32d58f76ae5efa2f1d1491482f472bb156799353
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-07 21:15:02 +0530

    display todo list

--- src/TodoLI.elm
+++ src/TodoLI.elm
@@ -1,6 +1,8 @@
 module TodoLI exposing (TodoDL, TodoLI, Transit(..), fromTodo, initDisplayList)
 
-import Todo exposing (Todo)
+import Set exposing (Set)
+import Todo exposing (Todo, TodoList)
+import TodoId exposing (TodoId)
 
 
 type Transit
@@ -25,6 +27,57 @@ fromTodo t =
     }
 
 
-initDisplayList : List Todo -> List TodoLI
+initDisplayList : TodoList -> TodoDL
 initDisplayList =
     List.map fromTodo
+
+
+toIdSet : TodoDL -> Set TodoId
+toIdSet todoDL =
+    todoDL |> List.map (.todo >> .id) |> Set.fromList
+
+
+update todoList todoDL =
+    let
+        newTodoDL : TodoDL
+        newTodoDL =
+            initDisplayList todoList
+
+        newIdSet : Set TodoId
+        newIdSet =
+            toIdSet newTodoDL
+
+        oldIdSet : Set TodoId
+        oldIdSet =
+            toIdSet todoDL
+
+        removedIdSet : Set TodoId
+        removedIdSet =
+            Set.diff oldIdSet newIdSet
+
+        removedTLI : List TodoLI
+        removedTLI =
+            todoDL
+                |> List.filter
+                    (\tli -> Set.member tli.todo.id removedIdSet)
+                |> List.map
+                    (\tli ->
+                        { tli
+                            | transit =
+                                case tli.transit of
+                                    Leaving _ ->
+                                        tli.transit
+
+                                    Entering _ ->
+                                        Leaving 0
+
+                                    Staying ->
+                                        Leaving 0
+                        }
+                    )
+
+        newTodoDLWithRemovedAndSorted : List TodoLI
+        newTodoDLWithRemovedAndSorted =
+            newTodoDL ++ removedTLI
+    in
+    1
