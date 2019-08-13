28d693cfd520e68b094dda208d6c2fb2330c11d5
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-13 15:00:24 +0530

    flip list demo

--- src/FlipList.elm
+++ src/FlipList.elm
@@ -1,6 +1,7 @@
 module FlipList exposing (FlipList, Msg, empty, init, update, view)
 
 import BasicsExtra exposing (callWith)
+import Browser.Dom as Dom exposing (Element)
 import FlipItem exposing (FlipItem)
 import Html.Styled exposing (Html, button, div, text)
 import Html.Styled.Attributes exposing (class)
@@ -11,6 +12,7 @@ import Random
 import Random.List
 import Result exposing (Result)
 import Result.Extra
+import Task
 import UpdateExtra exposing (pure)
 
 
@@ -34,6 +36,7 @@ type Msg
     | GotFlipItems (HttpResult (List FlipItem))
     | OnShuffle
     | GotRandomShuffled (List FlipItem)
+    | OnGotElement (Result Dom.Error Element)
 
 
 empty : FlipList
@@ -69,14 +72,25 @@ update message model =
         GotRandomShuffled fl ->
             onGotShuffled fl model
 
+        OnGotElement res ->
+            res
+                |> Result.Extra.unpack onDomError onGotElement
+                |> callWith model
+
 
 onGotShuffled shuffled model =
     case model of
         Stable fl ->
-            ( Flipping <| { from = fl, to = shuffled }, Cmd.none )
+            ( Flipping <| { from = fl, to = shuffled }
+            , Dom.getElement "foo" |> Task.attempt OnGotElement
+            )
 
         Flipping _ ->
             pure model
+
+
+onGotElement el model =
+    pure model
 
 
 onShuffle : FlipList -> Return
@@ -94,6 +108,15 @@ onHttpError err =
     let
         _ =
             Debug.log "HTTP Err" err
+    in
+    pure
+
+
+onDomError : Dom.Error -> FlipList -> Return
+onDomError err =
+    let
+        _ =
+            Debug.log "Dom Err" err
     in
     pure
 
