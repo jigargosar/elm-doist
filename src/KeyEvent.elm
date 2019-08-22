97180cee6823292e16c6531b9568eccadf8fde93
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-22 15:46:40 +0530

    todo menu

--- src/KeyEvent.elm
+++ src/KeyEvent.elm
@@ -1,8 +1,9 @@
-module KeyEvent exposing (onEnter)
+module KeyEvent exposing (enterKeyDecoder, onEnter, spaceKeyDecoder)
 
+import BasicsExtra exposing (ifElse)
 import Html exposing (Attribute)
 import Html.Events
-import Json.Decode as JD
+import Json.Decode as JD exposing (Decoder)
 
 
 onEnter : a -> Attribute a
@@ -19,3 +20,24 @@ onEnter tagger =
                             JD.fail "Not Interested"
                 )
         )
+
+
+keyDecoder : String -> a -> Decoder a
+keyDecoder targetKey tagger =
+    JD.at [ "key" ] JD.string
+        |> JD.andThen
+            (\key ->
+                ifElse (key == targetKey)
+                    (JD.succeed tagger)
+                    (JD.fail "")
+            )
+
+
+enterKeyDecoder : a -> Decoder a
+enterKeyDecoder =
+    keyDecoder "Enter"
+
+
+spaceKeyDecoder : a -> Decoder a
+spaceKeyDecoder =
+    keyDecoder " "
