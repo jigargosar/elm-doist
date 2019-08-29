7e95b484d6548de834f252fbcbcbe3391de6e0db
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-29 15:34:25 +0530

    key events

--- src/UI/Key.elm
+++ src/UI/Key.elm
@@ -1,4 +1,4 @@
-module UI.Key exposing (enter, escape, onDown, onKeyDownPreventDefault, pdOnKeydown, space)
+module UI.Key exposing (enter, escape, onDown, pdOnKeydown, space)
 
 import Accessibility.Styled.Key as Key
 import Html.Styled exposing (Attribute, Html)
@@ -9,21 +9,6 @@ import Json.Decode as JD exposing (Decoder)
 preventDefault : msg -> ( msg, Bool )
 preventDefault msg =
     Tuple.pair msg True
-
-
-onKeyDownPreventDefault : msg -> List (msg -> Decoder msg) -> Attribute msg
-onKeyDownPreventDefault msg keys =
-    let
-        decoder =
-            JD.lazy
-                (\_ ->
-                    keys
-                        |> List.map ((|>) msg)
-                        |> JD.oneOf
-                        |> JD.map preventDefault
-                )
-    in
-    preventDefaultOn "keydown" decoder
 
 
 pdOnKeydown : Decoder ( msg, Bool ) -> Attribute msg
