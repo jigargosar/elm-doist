50257a17f9049fc73c929919e9b9834350654651
Jigar Gosar <jigar.gosar@gmail.com>
2019-08-30 12:46:34 +0530

    revert extracting Msg

--- src/Main.elm
+++ src/Main.elm
@@ -38,7 +38,6 @@ import Json.Encode exposing (Value)
 import List.Extra
 import Maybe.Extra as MX
 import Millis exposing (Millis)
-import Msg exposing (..)
 import Ports exposing (FirestoreQueryResponse)
 import Project exposing (Project, ProjectList)
 import ProjectId exposing (ProjectId)
@@ -172,6 +171,47 @@ init encodedFlags url key =
 
 
 -- MSG
+
+
+type Msg
+    = NoOp
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url
+    | OnHere Time.Zone
+    | OnBrowserResize BrowserSize
+    | Focused (Result Dom.Error ())
+    | OnAuthStateChanged Value
+    | OnFirestoreQueryResponse FirestoreQueryResponse
+    | OnSignInClicked
+    | OnSignOutClicked
+      -- ExistingTodoOperations
+    | OnChecked TodoId Bool
+    | OnDelete TodoId
+    | PatchTodo TodoId (List Todo.Msg) Millis
+    | OnMoveStart TodoId
+    | OnTodoPopupTriggered TodoId
+    | OnTodoPopupMsg TodoPopup.Msg
+    | OnSchedulePopupTriggered SchedulePopup.Location TodoId
+    | OnSchedulePopupMsg SchedulePopup.Msg
+    | OnSchedulePopupClosed SchedulePopup.Location TodoId (Maybe DueAt)
+    | OnMoveToProject TodoId ProjectId
+    | OnDialogOverlayClickedOrEscapePressed
+    | EditTodoRequested TodoId
+    | TodoEditorTitleChanged TodoId String
+    | TodoEditorCanceled
+    | TodoEditorSaved
+      -- NewTodoOperations
+    | OnAddTodoStart ProjectId
+    | AddTodo ProjectId Millis
+    | OnAddTodoTodayStart
+    | AddTodoToday Millis
+      -- Project
+    | OnDeleteProject ProjectId
+    | OnAddProjectStart
+    | AddProject Millis
+
+
+
 -- SUB
 
 
