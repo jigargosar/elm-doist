d586995612724ded313e9ccd46960b9815426dd6
Jigar Gosar <jigar.gosar@gmail.com>
2019-09-03 13:07:27 +0530

    refactor Popup

--- src/MovePopup.elm
+++ src/MovePopup.elm
@@ -1,10 +1,11 @@
 module MovePopup exposing (Model(..), firstFocusable, initial, view)
 
 import BasicsExtra exposing (..)
-import Html.Styled exposing (Attribute, Html, div, text)
+import Html.Styled as H exposing (Attribute, Html, div, text)
 import Html.Styled.Attributes as A exposing (class, classList, tabindex)
-import Html.Styled.Events exposing (onClick)
-import MoveDialog
+import Html.Styled.Events exposing (on, onClick)
+import HtmlExtra as HX
+import Json.Decode as JD
 import Project exposing (Project, ProjectList)
 import ProjectId exposing (ProjectId)
 import TodoId exposing (TodoId)
@@ -41,40 +42,64 @@ inboxDisplayProject =
     { id = ProjectId.default, title = "Inbox" }
 
 
+toDisplayProjectList : List Project -> List DisplayProject
 toDisplayProjectList projectList =
     inboxDisplayProject :: List.map toDisplayProject projectList
 
 
-view : msg -> List (Html msg) -> Html msg
-view onOverlayClickOrEscapePressed content =
-    div
-        [ class "z-1 fixed absolute--fill flex items-center justify-center"
-        , Key.onEscape onOverlayClickOrEscapePressed
-        , tabindex -1
-        ]
-        [ div
-            [ class "absolute absolute--fill bg-black-50"
-            , onClick onOverlayClickOrEscapePressed
-            ]
-            []
-        , div [ class "absolute" ]
-            (let
-                viewProjectItem idx dp =
-                    TextButton.view (OpenMoveDialog todoId dp.id)
-                        dp.title
-                        [ class "ph3 pv2"
-                        , classList [ ( "b", dp.id == projectId ) ]
-                        , A.id <| ifElse (idx == 0) MoveDialog.firstFocusable ""
-                        ]
-             in
-             viewDialog
-                [ div [ class "bg-white pa3 lh-copy shadow-1" ]
-                    (div [ class "b" ] [ text "Move To Project ..." ]
-                        :: (projectList
-                                |> toDisplayProjectList
-                                |> List.indexedMap viewProjectItem
-                           )
-                    )
+isOpenForTodoId todoId model =
+    case model of
+        OpenFor todoId_ _ ->
+            True
+
+        Closed ->
+            False
+
+
+type alias ViewConfig msg =
+    { close : msg, move : TodoId -> ProjectId -> msg }
+
+
+view : ViewConfig msg -> TodoId -> List Project -> Model -> Html msg
+view config todoId projectList model =
+    case model of
+        OpenFor todoId_ projectId ->
+            if todoId /= todoId_ then
+                HX.none
+
+            else
+                viewHelp config todoId projectId projectList
+
+        Closed ->
+            HX.none
+
+
+viewHelp : ViewConfig msg -> TodoId -> ProjectId -> List Project -> Html msg
+viewHelp config todoId projectId projectList =
+    let
+        viewProjectItem idx dp =
+            TextButton.view (config.move todoId dp.id)
+                dp.title
+                [ class "ph3 pv2"
+                , classList [ ( "b", dp.id == projectId ) ]
+                , A.id <| ifElse (idx == 0) firstFocusable ""
                 ]
-            )
+    in
+    H.node "track-focus-outside"
+        [ class "absolute right-0 top-1"
+        , class "bg-white shadow-1 w5"
+        , class "z-1" -- if removed; causes flickering with hover icons
+        , tabindex -1
+        , on "focusOutside" (JD.succeed config.close)
+        , Key.onEscape config.close
+        ]
+        [ div [ class "bg-white pa3 lh-copy shadow-1" ]
+            [ div [ class "bg-white pa3 lh-copy shadow-1" ]
+                (div [ class "b" ] [ text "Move To Project ..." ]
+                    :: (projectList
+                            |> toDisplayProjectList
+                            |> List.indexedMap viewProjectItem
+                       )
+                )
+            ]
         ]
