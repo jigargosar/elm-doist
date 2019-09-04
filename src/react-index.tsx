f98b15abf4ae8e9d1accccd9d5f9c844257273dc
Jigar Gosar <jigar.gosar@gmail.com>
2019-09-04 13:30:44 +0530

    experimenting with react & typescript

--- src/react-index.tsx
+++ src/react-index.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
-import {render} from 'react-dom'
-import "tachyons"
-import "./index.css"
+import React, { useState } from 'react'
+import { render } from 'react-dom'
+import 'tachyons'
+import './index.css'
+import nanoid from 'nanoid'
+import faker from 'faker'
+import times from 'ramda/es/times'
 
-function App(){
-  return <div>HW</div>
+type Todo = {
+  id: string
+  title: string
 }
 
+type State = {
+  todoList: Todo[]
+}
 
-render (<App/>, document.getElementById("root"))
+function createFakeTodo() {
+  return { id: nanoid(), title: faker.hacker.phrase() }
+}
+
+const initialTodos: Todo[] = times(createFakeTodo, 10)
+
+const initialState: State = { todoList: initialTodos }
+
+function App() {
+  const [state, setState] = useState(initialState)
+
+  return (
+    <div>
+      <div>TodoList</div>
+      {state.todoList.map(todo => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </div>
+  )
+}
+
+function TodoItem({ todo }: { todo: Todo }) {
+  return <div className="flex">{todo.title}</div>
+}
+
+render(<App />, document.getElementById('root'))
