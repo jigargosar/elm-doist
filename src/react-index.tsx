761a765b39c02ec36ec6940dea0a8ba4c59c4d52
Jigar Gosar <jigar.gosar@gmail.com>
2019-09-04 14:22:55 +0530

    experimenting with react & typescript

--- src/react-index.tsx
+++ src/react-index.tsx
@@ -11,7 +11,8 @@ type Todo = {
   title: string
 }
 
-type State = {
+type Model = {
+  todoPopup: { tag: 'Closed' } | { tag: 'Open'; todoId: string }
   todoList: Todo[]
 }
 
@@ -21,13 +22,34 @@ function createFakeTodo() {
 
 const initialTodos: Todo[] = times(createFakeTodo, 10)
 
-const initialState: State = { todoList: initialTodos }
+const initialModel: Model = {
+  todoPopup: { tag: 'Closed' },
+  todoList: initialTodos,
+}
+
+function exhaustiveCheck(never: never) {
+  return never
+}
+
+type Msg =
+  | { tag: 'OpenTodoPopup'; payload: [] }
+  | { tag: 'Check'; payload: [string, boolean] }
+
+function update(msg: Msg, model: Model) {
+  if (msg.tag === 'OpenTodoPopup') {
+    return model
+  } else if (msg.tag === 'Check') {
+    const [todoId, isChecked] = msg.payload
+    return model
+  }
+  return exhaustiveCheck(msg)
+}
 
 function App() {
-  const [state] = useState(initialState)
+  const [state] = useState(initialModel)
 
   return (
-    <div className="lh-copy" style={{maxWidth: 500}}>
+    <div className="lh-copy" style={{ maxWidth: 500 }}>
       <div className="f4 pv1">TodoList</div>
       {state.todoList.map(todo => (
         <TodoItem key={todo.id} todo={todo} />
@@ -40,7 +62,11 @@ function TodoItem({ todo }: { todo: Todo }) {
   return (
     <div className="flex">
       <div className="ph1 pv2">
-        <input type="checkbox" className="" style={{width:24, height:24}}/>
+        <input
+          type="checkbox"
+          className=""
+          style={{ width: 24, height: 24 }}
+        />
       </div>
       <div className="ph1 pv1 flex-grow-1 lh-title ">{todo.title}</div>
       <div className="ph1 b pointer">...</div>
